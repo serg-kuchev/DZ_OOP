c0ddc03e4f3277cad057592196c6a411ca9e3cf2
Сергей Кучев <kuchevwork@yandex.ru>
2020-11-30 23:56:00 +0300

    Программа отправки файлы в яндекс диск

---
+++ yadisk.py
@@ -0,0 +1,23 @@
+from pprint import pprint
+
+import requests
+
+HEADERS = {'Authorization': 'OAuth '}
+
+resp = requests.get(
+    'https://cloud-api.yandex.net/v1/disk/resources/upload',
+    params={'path': 'VL_Kursach.docx', 'overwrite': 'true'},
+    headers=HEADERS
+)
+
+resp.raise_for_status()
+d = resp.json()
+
+href = d['href']
+
+with open('VL_Kursach.docx', 'rb') as f:
+    resp2 = requests.put(href, files={'file': f})
+
+resp2.raise_for_status()
+
+pprint(d)
